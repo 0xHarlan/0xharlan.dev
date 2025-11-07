99ab3e12f041086eaeea260271ce99c915314a78
Guives <guives.richard@etu.u-paris.fr>
2025-11-07 19:37:22 +0100

    Base layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,35 @@
 <script>
-  let { children } = $props();
   import "../app.css";
+  import { page } from '$app/stores';
+
+  const links = [
+    { name: 'About', href: '/about' },
+    { name: 'Projects', href: '/projects' }
+  ];
+
+  let { children } = $props();
 </script>
 
-{@render children()}
+
+<header class=""> 
+  <div class="flex justify-between items-start">
+    <a href="/" class="text-2xl font-bold">Firstname Surname</a>
+
+    <nav class="flex space-x-4">
+      {#each links as link} 
+        <a 
+          href="{link.href}"
+          class:text-slate-400={$page.url.pathname === link.href}>
+          {link.name}
+        </a>
+      {/each}
+    </nav>
+  </div>
+</header>
+
+<main class="">
+  {@render children()} <!-- Renders the current page -->
+</main>
+
+<footer></footer>
+
---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,4 @@
+<h1 class="text-3xl font-bold underline">
+  Projects
+</h1>
+<p>Lorem Ipsum</p>
